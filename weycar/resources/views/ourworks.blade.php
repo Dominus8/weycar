aec7430230789c59a5f485c7f50e38a28ba68f25
Dominus8 <ikari162@mail.ru>
2021-12-28 13:04:12 +0300

    WEYC-34
    https://weycar.atlassian.net/browse/WEYC-34

--- weycar/resources/views/ourworks.blade.php
+++ weycar/resources/views/ourworks.blade.php
@@ -50,11 +50,20 @@
                                                     </div> 
                                                     <div class="swiper card-slider__block slider-block{{$slide->id}}">
                                                         <div class="swiper-wrapper">
-                                                        @foreach($slide -> owimage as $image)
+                                                        <!-- @foreach($slide -> owimage as $image)
                                                             <div class="swiper-slide">
                                                                 <img style= "width: 270px;height: 230px;" src="/storage/owimage/{{$image}}" class="rounded" alt="{{$slide->owalt}}">
                                                             </div>
-                                                        @endforeach
+                                                        @endforeach -->
+                                                        @if(is_array($slide->owimage))
+                                                            @foreach($slide->owimage as $i)
+                                                              <div style="cursor: zoom-in" class="cards-image-container swiper-slide" type="button" data-bs-toggle="modal" data-bs-target="#imageModal{{$slide->id}}">
+
+                                                                  <img src="/storage/owimage/{{$i}}">
+
+                                                              </div>
+                                                            @endforeach
+                                                        @endif
                                                         </div>
                                                     </div>
                                                 </div>
@@ -62,6 +71,46 @@
                                         </div>
                                     </div>
                                 </div>
+                            </div>
+
+                                            <!-- Modal с картинками -->
+                            <div class="modal modal-image-slider fade" data-local="#myCarousel" id="imageModal{{$slide->id}}" style="top:20px" tabindex="-1" aria-labelledby="imageModal{{$slide->id}}Label" aria-hidden="true">
+                              <div class="modal-dialog modal-dialog-centered modal-xl ">
+                                <div class="modal-content">
+                                  <div class="modal-header">
+                                    <h5 class="modal-title" id="imageModal{{$slide->id}}Label">Изображения</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                  </div>
+                                  <div class="modal-body">
+                                    <!-- Slider main container -->
+                                    <div class="swiper slider-init-container swiper{{$slide->id}}" >
+                                      <!-- Additional required wrapper -->
+                                      <div class="swiper-wrapper" >
+                                        <!-- Slides -->
+            
+                                        @if(is_array($slide->owimage))
+                                            @foreach($slide->owimage as $i)
+            
+                                            <div class="swiper-slide">
+                                              <img src="/storage/owimage/{{$i}}">
+                                            </div>   
+            
+                                            @endforeach
+                                        @endif
+                                      </div>
+                                      <!-- If we need pagination -->
+                                      <div class="swiper-pagination"></div>
+            
+                                      <!-- If we need navigation buttons -->
+                                      <div class="swiper-button-prev"></div>
+                                      <div class="swiper-button-next"></div>
+            
+                                      <!-- If we need scrollbar -->
+                                      <!-- <div class="swiper-scrollbar"></div> -->
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
                             </div>
                             <script>
                                 $( document ).ready(function() {
@@ -80,9 +129,31 @@
                                             mySwiper{{$slide->id}}.slideTo(index);
                                         })
                                     });
-                                });    
+                                });
+
+                                // Слайдер в модалке
+
+                                const swiperimg{{$slide->id}} = new Swiper('.swiper{{$slide->id}}', {
+
+                                  loop: true,
+
+                                  pagination: {
+                                    el: '.swiper-pagination',
+                                  },
+                              
+                                  navigation: {
+                                    nextEl: '.swiper-button-next',
+                                    prevEl: '.swiper-button-prev',
+                                  },
+                              
+                                  scrollbar: {
+                                    el: '.swiper-scrollbar',
+                                  },
+                                }); 
                             </script>
-                        </div>                            
+                        </div>
+                        
+
                             @endforeach
                         </div>    
                     </div>
