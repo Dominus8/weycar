5f52a599e77f5cd6ce69559e583fd8cf33082763
Dominus8 <ikari162@gmail.com>
2022-01-26 18:57:41 +0300

    https://weycar.atlassian.net/browse/WEYC-29

--- weycar/resources/views/base.blade.php
+++ weycar/resources/views/base.blade.php
@@ -24,6 +24,7 @@
     <link rel="stylesheet" href="/css/services.css">
     <link rel="stylesheet" href="/css/nashi-raboty.css">
     <link rel="stylesheet" href="/css/singl-page.css">
+    <link rel="stylesheet" href="/css/animate.min.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
     <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script>
